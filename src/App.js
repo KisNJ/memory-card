e7473c9e9795689a59dad619f87d22b1386a84f8
kisnj <knandor407@gmail.com>
2022-07-04 15:45:59 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import cardsrc from "./cardsrc.json";
+import "./App.css";
 
+import { useEffect, useState } from "react";
 function App() {
+  const[cardsToDisplay,setCardsToDisplay]=useState([])
+  useEffect(() => {
+    let arr = [];
+    while (arr.length < 5) {
+      let r = Math.floor(Math.random() * 33);
+      if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    console.log(cardsrc.forEach(card=>console.log(card)))
+    let temp=arr.map(id=>{
+      return cardsrc.find(card=>parseInt(card.id)===parseInt(id))
+    })
+
+    setCardsToDisplay([...temp])
+  }, []);
+  //"./imgs/10000000 - Falibor.png"
+  // '10020000 - Arachas Venom'
+  // '10020000 - Arachas Venom.png'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    {cardsToDisplay.map(x=>{
+      let src=`${x.srcId} - ${x.title}.png`
+      return <img alt='gwent card' src={require(`./imgs/${src}`)}/>
+    })}
+  </div>)
 }
 
 export default App;
